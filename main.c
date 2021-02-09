7b9e705fcee54644f8fa584abe1c082665e463da
mandonnetj <julien.mandonnet@etu.umontpellier.fr>
2021-02-09 22:05:22 +0000

    Ajout Question 2 à 5

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int maximum(int *t, int n);
+void f(int a, int b, int *s, int *p);
+void minmax(int *t, int n, int *min, int *max);
 void stl();
 
 
@@ -8,14 +10,25 @@ void stl();
 
 
 int main() {
-  int t[5] = {1, 8, 3, 4, 6};
+  int t[10] = {1, 8, 3, -14, 6, -29, 78, -6, 24, -10};
+  int p;
+  int s;
+  int min;
+  int max;
   stl();
 
   printf("Hello World !\n");
   stl();
 
-  printf("%i\n", maximum(t, 5));
+  printf("%i\n", maximum(t, 10));
   stl();
+
+  f( 5, 7, &s, &p);
+  printf("somme: %i\nproduit: %i\n", s, p);
+  stl();
+
+  minmax( t, 10, &min, &max);
+  printf("min: %i\nmax: %i\n", min, max);
 
 }
 
@@ -32,6 +45,23 @@ int maximum(int *t, int n) {
   }
   return m;
 
+}
+
+void f(int a, int b, int *s, int *p) {
+  *s = a + b;
+  *p = a * b;
+}
+
+void minmax(int *t, int n, int *pmin, int *pmax) {
+  *pmin = t[0];
+  *pmax = t[0];
+  for(int i=0; i<n; i++){
+    if( t[i] > *pmax) {
+      *pmax = t[i]; 
+    }else if( t[i] < *pmin) {
+      *pmin = t[i]; 
+    }
+  }
 }
 
 void stl() {
